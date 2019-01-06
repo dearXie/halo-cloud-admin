221277c4e40228eb630d84060554dbc282c7199e
xujin <Softwar_King@qq.com>
2019-01-06 18:49:16 +0800

    封装统一的返回对象

--- spring-cloud-admin-extension/src/main/java/cn/springcloud/admin/extension/AppController.java
+++ spring-cloud-admin-extension/src/main/java/cn/springcloud/admin/extension/AppController.java
@@ -1,5 +1,6 @@
 package cn.springcloud.admin.extension;
 
+import cn.springcloud.admin.common.ResultData;
 import de.codecentric.boot.admin.server.services.InstanceRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,7 @@ public class AppController {
     private InstanceRegistry registry;
 
     @GetMapping(path = "/applications")
-    public String applications() {
-        return "sss";
+    public ResultData applications() {
+        return ResultData.builder().data("返回的Data").build();
     }
 }
